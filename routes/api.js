09a60cfeedc6a35d46688be0427ca2f589cd60fd
yeongcoder <tlsqnsla15@naver.com>
2019-07-30 12:04:13 +0900

    연결된 답변중 랜덤하게 표현 및 예외 답변 추가

--- routes/api.js
+++ routes/api.js
@@ -82,33 +82,41 @@ module.exports = (app) => {
                         if(data == null) { //예상 질의와 일치하지 않는 경우
                             let input_code = Math.random().toString()+Date.now();
                             
-                            sentence.input_text = userText;
-                            sentence.en_sentence = en_sentence;
-                            sentence.ko_sentence = null;
-                            sentence.input_code = input_code;
-                            sentence.keyword = en_sentence.split(' ');
+                            request({
+                                uri: `${uri}/exception/get/detail`,
+                                method: 'GET',
+                                body: {
+                                    "exception_code": "021"
+                                },
+                                json: true  
+                            }).then(exception => {
+                                
+                                sentence.input_text = userText;
+                                sentence.en_sentence = en_sentence;
+                                sentence.ko_sentence = null;
+                                sentence.input_code = input_code;
+                                sentence.keyword = en_sentence.split(' ');
+    
+                                history.user_id = userId;
+                                history.user_tag = null;
+                                history.input_text = userText;
+                                history.en_sentence = en_sentence;
+                                history.ko_sentence = null;
+                                history.input_code = input_code;
+                                history.bot_text = exception.exception_text;
+    
+                                save_sentence(uri, sentence);
+                                save_history(uri, history);
+    
+                                res.status(200).send(exception.exception_text);
+                                console.log(exception.exception_script);
 
-                            history.user_id = userId;
-                            history.user_tag = null;
-                            history.input_text = userText;
-                            history.en_sentence = en_sentence;
-                            history.ko_sentence = null;
-                            history.input_code = input_code;
-                            history.bot_text = "2차 가공후 예상질의에 없어서 추가함";
-                            history.exception_text = null;
-
-                            save_sentence(uri, sentence);
-                            // save_manage(uri, manage);
-                            save_history(uri, history);
-                            res.status(200).send("2차 가공후 예상질의에 없어서 추가함");
-
-                            console.log(sentence);
-                            console.log(history);
+                            })
                             
                         } else { //예상 질의와 일치한 경우
 
                             return request({
-                                uri: `${uri}/answer/get/detail`,
+                                uri: `${uri}/answer/get/list`,
                                 method: 'GET',
                                 body: {
                                     "input_code": data.input_code
@@ -119,29 +127,40 @@ module.exports = (app) => {
                                 if(data == null) { //답변이 없는 경우
                                     let input_code = Math.random().toString()+Date.now();
 
-                                    sentence.input_text = userText;
-                                    sentence.en_sentence = en_sentence;
-                                    sentence.ko_sentence = null;
-                                    sentence.input_code = input_code;
-                                    sentence.keyword = en_sentence.split(' ');
-        
-                                    history.user_id = userId;
-                                    history.user_tag = null;
-                                    history.input_text = userText;
-                                    history.en_sentence = en_sentence;
-                                    history.ko_sentence = null;
-                                    history.input_code = input_code;
-                                    history.bot_text = "2차 가공후 예상질의에는 있지만 답변이 존재하지않음";
-                                    history.exception_text = null;
-        
-                                    save_sentence(uri, sentence);
-                                    save_history(uri, history);
+                                    request({
+                                        uri: `${uri}/exception/get/detail`,
+                                        method: 'GET',
+                                        body: {
+                                            "exception_code": "02"
+                                        },
+                                        json: true  
+                                    }).then(exception => {
 
-                                    res.status(200).send("2차 가공후 예상질의에는 있지만 답변이 존재하지않음");
+                                        sentence.input_text = userText;
+                                        sentence.en_sentence = en_sentence;
+                                        sentence.ko_sentence = null;
+                                        sentence.input_code = input_code;
+                                        sentence.keyword = en_sentence.split(' ');
+            
+                                        history.user_id = userId;
+                                        history.user_tag = null;
+                                        history.input_text = userText;
+                                        history.en_sentence = en_sentence;
+                                        history.ko_sentence = null;
+                                        history.input_code = input_code;
+                                        history.bot_text = exception.exception_text;
+            
+                                        save_sentence(uri, sentence);
+                                        save_history(uri, history);
+    
+                                        res.status(200).send(exception.exception_text);
+                                        console.log(exception.exception_script);
+
+                                    });
 
                                 } else { //답변이 있는 경우
-
-                                    let input_code = data._id;
+                                    let randomAnswer = data[Math.floor(Math.random() * data.length)];
+                                    let input_code = data.input_code;
 
                                     sentence.input_text = userText;
                                     sentence.en_sentence = en_sentence;
@@ -155,13 +174,12 @@ module.exports = (app) => {
                                     history.en_sentence = en_sentence;
                                     history.ko_sentence = null;
                                     history.input_code = input_code;
-                                    history.bot_text = data.answer_text_ko;
-                                    history.exception_text = null;
+                                    history.bot_text = randomAnswer.answer_text_ko;
         
                                     save_sentence(uri, sentence);
                                     save_history(uri, history);
 
-                                    res.status(200).send(data.answer_text_ko);
+                                    res.status(200).send(randomAnswer.answer_text_ko);
 
                                 }
                                 
@@ -175,7 +193,7 @@ module.exports = (app) => {
             } else { //예상 질의와 일치한 경우
 
                 return request({
-                    uri: `${uri}/answer/get/detail`,
+                    uri: `${uri}/answer/get/list`,
                     method: 'GET',
                     body: {
                         "input_code": data.input_code
@@ -186,22 +204,33 @@ module.exports = (app) => {
 
                     if(data == null) { //예상 질의에 해당하는 답변이 없는경우
 
-                        history.user_id = userId;
-                        history.user_tag = null;
-                        history.input_text = userText;
-                        history.en_sentence = null;
-                        history.ko_sentence = null;
-                        history.input_code = null;
-                        history.bot_text = "1차 가공후 예상질의에는 있지만 답변이 존재하지않음";
-                        history.exception_text = null;
+                        request({
+                            uri: `${uri}/exception/get/detail`,
+                            method: 'GET',
+                            body: {
+                                "exception_code": "01"
+                            },
+                            json: true  
+                        }).then(exception => {
 
-                        save_history(uri, history);
-            
-                        res.status(200).send("1차 가공후 예상질의에는 있지만 답변이 존재하지않음");//예외처리
+                            history.user_id = userId;
+                            history.user_tag = null;
+                            history.input_text = userText;
+                            history.en_sentence = null;
+                            history.ko_sentence = null;
+                            history.input_code = null;
+                            history.bot_text = exception.exception_text;
+    
+                            save_history(uri, history);
+                
+                            res.status(200).send(exception.exception_text);
+                            console.log(exception.exception_script);
+
+                        });
 
                     } else { //예상 질의에 해당하는 답변이 있는 경우
-
-                        let input_code =  data._id;
+                        let randomAnswer = data[Math.floor(Math.random() * data.length)];
+                        let input_code =  data.input_code;
                     
                         history.user_id = userId;
                         history.user_tag = null;
@@ -209,12 +238,11 @@ module.exports = (app) => {
                         history.en_sentence = null;
                         history.ko_sentence = null;
                         history.input_code = input_code;
-                        history.bot_text = data.answer_text_ko;
-                        history.exception_text = null;
+                        history.bot_text = randomAnswer.answer_text_ko;
 
                         save_history(uri, history);//[DB추가(History Collection)]
                     
-                        res.status(200).send(data.answer_text_ko);//답변
+                        res.status(200).send(randomAnswer.answer_text_ko);//답변
 
                     }
 
@@ -225,57 +253,6 @@ module.exports = (app) => {
         .catch(err => {
             console.error(err);
         })
-
-        // /////promise 분기 로직//////
-        // //02.[DB매칭(Sentence Collection)] 사용자가 입력한 텍스트가 예상 질의에 있는지 탐색
-        // await request({ 
-        //     uri: `${uri}/sentence/get/detail`,
-        //     method: 'GET',
-        //     body: {"input_text": input_text},
-        //     json: true
-        // })
-        // .then(data => { 
-        //     step01_data = data;
-        // })
-        // .catch(err => {
-        //     console.error(err);
-        // })
-
-        // //03.(NO) -> 예상 질의에 없는 경우 2차 가공 로직으로 이동
-        // if(step01_data == null) { 
-        //     //2차 가공 로직
-        // }
-
-        // //03.(YES) -> [DB매칭(Answer Collection)] 예상 질의에 해당하는 답변이 있는지 탐색
-        // await request({
-        //     uri: `${uri}/answer/get/detail`,
-        //     method: 'GET',
-        //     body: {"input_code": step01_data.input_code},
-        //     json: true  
-        // })
-        // .then(data => {
-        //     history.input_code = step01_data.input_code;//history 업데이트
-        //     step02_data = data;
-        // })
-        // .catch(err => {
-        //     console.error(err);
-        // })
-
-        // //04.(NO) -> 답변탐색 결과가 없는 경우 예외 처리
-        // if(step02_data == null) { 
-        //     history.exception_text = "DB에서 탐색한 예외문장 추가";//history 업데이트
-        //     save_history(uri, history);//[DB추가(History Collection)]
-   
-        //     res.status(200).send("아직 못배운 말이에요");//예외처리
-        //     return;
-        // }
-        
-        // //04.(YES) -> 답변탐색 결과가 있는 경우 답변 출력
-        // history.bot_text = step02_data.answer_text_ko;//history 업데이트
-        // save_history(uri, history);//[DB추가(History Collection)]
-    
-        // res.status(200).send(step02_data.answer_text_ko);//답변처리
-
 
 
         //////////////////////로컬 함수 모음////////////////////////
